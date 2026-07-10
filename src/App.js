0ea25ec6940f93d92470216dae73659d7f819b9f
= <traviter@gmail.com>
2026-07-10 10:47:58 -0700

    85

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Home from "./routes/home/home.component";
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home />}>
+        <Route path='shop' element={<h1>Test shop</h1>} />
+      </Route>
     </Routes>
   );
 };
