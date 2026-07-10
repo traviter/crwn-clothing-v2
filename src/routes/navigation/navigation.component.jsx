28e226a2aa5d9db11db7960b699b2d75a8bf2b53
= <traviter@gmail.com>
2026-07-10 11:10:41 -0700

    88

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from 'react'
 import { Outlet, Link } from 'react-router-dom'
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss'
 
 const Navigation = () => {
     return (
         <Fragment>
             <div className='navigation'>
                 <Link className='nav-link' to='/'>
-                    <div>Logo</div>
+                    <CrownLogo className='logo-container' />
                 </Link>
                 <div className='nav-links-container'>
                     <Link className='nav-link' to='/shop'>
