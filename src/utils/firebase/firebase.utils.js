02d94daa3c028364c7962ee3337d24663e1c12c3
= <traviter@gmail.com>
2026-07-10 14:23:37 -0700

    95 Setting up User Documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,13 @@ import {
     GoogleAuthProvider,
 } from 'firebase/auth';
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDR63r6k9uadchO4XbEF3XIuZEwlmzFNfk",
@@ -28,3 +35,14 @@ authProvider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, authProvider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+
+    console.log(userSnapshot.exists());
+}
