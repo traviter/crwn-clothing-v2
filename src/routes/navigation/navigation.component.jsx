f9ed9b5078f6f36bb9529307f8fde5c36dce279d
= <traviter@gmail.com>
2026-07-10 23:23:52 -0700

    116 Cart Icon & Dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,6 +7,9 @@ import { signOutUser } from '../../utils/firebase/firebase.utils'
 
 import { UserContext } from '../../contexts/user.context'
 
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
+
 const Navigation = () => {
     const { currentUser, setCurrentUser } = useContext(UserContext);
     console.log(currentUser);
@@ -37,7 +40,9 @@ const Navigation = () => {
                             </Link>
                         )
                     }
+                    <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </Fragment>
