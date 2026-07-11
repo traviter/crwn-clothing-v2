2a138339980f41190179780d26d00127bb5a0ed4
= <traviter@gmail.com>
2026-07-11 12:39:11 -0700

    133 Category Preview Component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,19 +1,22 @@
-import { useContext } from 'react'
+import { useContext, Fragment } from 'react'
 import { CategoriesContext } from "../../contexts/categories.context";
 
 import './shop.styles.scss'
 
-import ProductCard from '../../components/product-card/product-card.component';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 const Shop = () => {
     const { categoriesMap } = useContext(CategoriesContext);
-    const products = categoriesMap['hats'] ?? [];
+
     return (
-        <div className='products-container'>
+        <div className='shop-container'>
             {
-                products
-                    .map((product) => (<ProductCard key={product.id} product={product} />)
-                    )
+                Object.keys(categoriesMap).map(
+                    (title) => {
+                        const products = categoriesMap[title];
+                        return <CategoryPreview key={title} title={title} products={products} />;
+                    }
+                )
             }
         </div>
     )
